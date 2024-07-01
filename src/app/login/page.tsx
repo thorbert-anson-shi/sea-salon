f9587440753fad8a5d67adaa4d7e494f32d432c3
Thorbert1 <tobyas2005139@gmail.com>
2024-07-01 23:47:47 +0700

    Added booking system

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export function LoginPage() {
-  return (
-    <div className="flex h-screen w-screen flex-col">
-      <form className="flex flex-col">div*</form>
-    </div>
-  );
-}
-
-export default LoginPage;
